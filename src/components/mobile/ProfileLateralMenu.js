f4e4cc657be19f31768ba24a61b6997347baa2d9
nilde <Nil.Domene@e-campus.uab.cat>
2022-09-17 17:00:38 +0200

    Fixed lateral menu animation

--- src/components/mobile/ProfileLateralMenu.js
+++ src/components/mobile/ProfileLateralMenu.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lateralOffset: new Animated.Value(100000),
+      lateralOffset: new Animated.Value(-100000),
       widthScreen:window.innerWidth,
       heightScreen:window.innerHeight
     }
